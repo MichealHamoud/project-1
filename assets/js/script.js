c5cc6e42e484febbc44d1aa2a85a1570dd1bb63e
MichealHamoud <micheal.hamoud11@outlook.com>
2023-03-30 19:23:15 +1100

    added the some functions for the search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,99 @@
-//get token api
+var userFormEl = document.querySelector('#user-form');
+var artistInputEl = document.querySelector('#artist');
+var songContainerEl = document.querySelector('#song-container');
+var artistSearchTerm = document.querySelector('#artist-search-term');
+var songButtonEl = document.querySelector('#song-button');
 
-var client_id = '44c2ad3160174fc089bfbe272aa6eb71';
-var client_secret = '5d75f019233b4757a4de12db7680508c';
+var formSubmitHandler = function (event) {
+  event.preventDefault();
 
-var authOptions = {
-  url: 'https://accounts.spotify.com/api/token',
-  headers: {
-    'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
-  },
-  form: {
-    grant_type: 'client_credentials'
-  },
-  json: true
+  var username = artistInputEl.value.trim();
+
+  if (username) {
+    getArtist(username);
+
+    songContainerEl.textContent = '';
+    artistInputEl.value = '';
+  } 
 };
 
-request.post(authOptions, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-    var token = body.access_token;
+//var buttonClickHandler = function (event) {
+  // `event.target` is a reference to the DOM element of what programming language button was clicked on the page
+  //var language = event.target.getAttribute('data-language');
+
+  // If there is no language read from the button, don't attempt to fetch repos
+  //if (language) {
+  //  getFeaturedRepos(language);
+
+   // songContainerEl.textContent = '';
+  //}
+//};
+
+var getArtist = function (user) {
+  var apiUrl = 'https://api.spotify.com/v1/search?q=name:' + song + '%20artist:' + artist + '&type=track&limit=10';
+
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          displayArtist(data, user);
+        });
+      } 
+    });
+};
+
+var getFeaturedArtist = function (language) {
+  // The `q` parameter is what language we want to query, the `+is:featured` flag adds a filter to return only featured repositories
+  // The `sort` parameter will instruct GitHub to respond with all of the repositories in order by the number of issues needing help
+  var apiUrl = 'https://api.spotify.com/v1/search?q=name:' + song + '%20artist:' + artist + '&type=track&limit=10';
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayArtist(data.items, language);
+      });
+    } 
+  });
+};
+
+var displayArtist = function (artist, searchTerm) {
+  if (artist.length === 0) {
+    songContainerEl.textContent = 'No repositories found.';
+    // Without a `return` statement, the rest of this function will continue to run and perhaps throw an error if `repos` is empty
+    return;
   }
-});
+
+  artistSearchTerm.textContent = searchTerm;
+
+  for (var i = 0; i < artist.length; i++) {
+    // The result will be `<github-username>/<github-repository-name>`
+    var artistName = artist[i].name + '/' + artist[i].song;
+
+    var artistEl = document.createElement('div');
+    artistEl.classList = 'list-item flex-row justify-space-between align-center';
+
+    var titleEl = document.createElement('span');
+    titleEl.textContent = artistName;
+
+    artistEl.appendChild(titleEl);
+
+    var statusEl = document.createElement('span');
+    statusEl.classList = 'flex-row align-center';
+
+    if (artist[i].open_issues_count > 0) {
+      statusEl.innerHTML =
+        "<i class='fas fa-times status-icon icon-danger'></i>" + artist[i].open_issues_count + ' issue(s)';
+    } else {
+      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
+
+    artistEl.appendChild(statusEl);
+
+    songContainerEl.appendChild(repoEl);
+  }
+};
+
+userFormEl.addEventListener('submit', formSubmitHandler);
+//songButtonEl.addEventListener('click', buttonClickHandler);
--- assets/js/token.js
+++ assets/js/token.js
@@ -1,22 +1,23 @@
-const APIController = (function(){
-
-    const clientId ='44c2ad3160174fc089bfbe272aa6eb71';
+const clientId = '44c2ad3160174fc089bfbe272aa6eb71';
     const clientSecret = '5d75f019233b4757a4de12db7680508c';
+    let token;
 
-        // private methods
-        const _getToken = async () => {
 
-            const result = await fetch('https://accounts.spotify.com/api/token', {
-                method: 'POST',
-                headers: {
-                    'Content-Type' : 'application/x-www-form-urlencoded', 
-                    'Authorization' : 'Basic ' + btoa( clientId + ':' + clientSecret)
-                },
-                body: 'grant_type=client_credentials'
-            });
-    
-            const data = await result.json();
-            return data.access_token;
-        }
-})
+    // private methods
+    const _getToken = async () => {
+
+        const result = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/x-www-form-urlencoded', 
+                'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
+            },
+            body: 'grant_type=client_credentials'
+        });
+
+        const data = await result.json();
+        token= (data.access_token);
+        console.log(token);
+        return data.access_token;
+    }
 
